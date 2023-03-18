814c2249279e54f3b199ab923e346b8785a157c3
kai-franz <kfranz@andrew.cmu.edu>
2023-03-18 17:53:03 -0400

    Formatting

--- src/PLPgSQL/Scalar UDFs/sudf_13_maxPurchaseChannel.sql
+++ src/PLPgSQL/Scalar UDFs/sudf_13_maxPurchaseChannel.sql
@@ -49,4 +49,4 @@ $$;
 SELECT c_customer_sk,
        maxPurchaseChannel(c_customer_sk, (SELECT MIN(d_date_sk) FROM date_dim WHERE d_year = 2000),
                           (SELECT MAX(d_date_sk) FROM date_dim WHERE d_year = 2020)) AS channel
-  FROM customer
+  FROM customer;
