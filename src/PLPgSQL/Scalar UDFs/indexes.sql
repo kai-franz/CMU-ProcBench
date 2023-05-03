f50a9d018d1376ebdfdd0bee9ebcbb01cbbd5f75
kai <kfranz@andrew.cmu.edu>
2023-05-03 19:52:18 -0400

    DROP INDEX statements

---
+++ src/PLPgSQL/Scalar UDFs/indexes.sql
@@ -0,0 +1,8 @@
+DROP INDEX IF EXISTS customer_address_ca_state_ca_address_sk_idx;
+DROP INDEX IF EXISTS time_dim_hour_time_sk_idx;
+DROP INDEX IF EXISTS web_sales_history_item_ext_discount_amt;
+DROP INDEX IF EXISTS date_dim_year;
+DROP INDEX IF EXISTS web_sales_history_ws_bill_customer_sk_ws_sold_date_sk;
+DROP INDEX IF EXISTS store_sales_history_ss_customer_sk_ss_sold_date_idx;
+DROP INDEX IF EXISTS catalog_sales_history_cs_bill_customer_sk_cs_sold_date_idx;
+DROP INDEX IF EXISTS web_sales_history_ws_bill_customer_sk_ws_sold_date_idx;
